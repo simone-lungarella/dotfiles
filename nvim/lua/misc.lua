61e3b99c03d1f95c4fa348e61c21d1aa185028bc
simone-lungarella <simonelungarella@gmail.com>
2024-12-27 11:56:14 +0100

    feat: changed yanked text highlight color

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -1,10 +1,12 @@
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#fab387", fg = "#000000", bold = true })
+
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 100 })
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
