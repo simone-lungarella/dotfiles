5a743994459ffa58ab701634bf966ac7cc135b83
Simone Lungarella <simonelungarella@gmail.com>
2025-07-31 11:25:16 +0200

    fix(nvim): added setting to ingnorecase in search mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.breakindent = true
 vim.o.undofile = true
+vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.swapfile = false
 vim.o.termguicolors = true
